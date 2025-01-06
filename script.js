2ec34c5319b31340ab502f9da98bac665a3dd382
Abdullah <68245138+Abdullah1856@users.noreply.github.com>
2025-01-06 10:25:31 -0800

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,8 @@ const btnClose = document.querySelector('.close-modal');
 const openModel = document.querySelectorAll('.show-modal');
 console.log(openModel);
 
-for(let i = 0; i < openModel.length; i++)
-    openModel[i].addEventListener('click', function() {
-// document.querySelector('.hidden').style.display = 'block';
-modal.classList.remove('hidden');
-// console.log('Btn is clicked!');
-
+for (let i = 0; i < openModel.length; i++)
+    openModel[i].addEventListener('click', function () {
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
     })
