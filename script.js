aacc003132234ce095f79d064d837571591704c3
Abdullah <68245138+Abdullah1856@users.noreply.github.com>
2025-01-07 15:17:00 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,20 +4,26 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnClose = document.querySelector('.close-modal');
 const openModal = document.querySelectorAll('.show-modal');
-console.log(openModal);
 
-function closeModel() {
+function closeModal() {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 }
 
-function openModels() {
+function openModals() {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 }
 
 for (let i = 0; i < openModal.length; i++)
-  openModal[i].addEventListener('click', openModels);
+  openModal[i].addEventListener('click', openModals);
 
-btnClose.addEventListener('click', closeModel);
-overlay.addEventListener('click', closeModel);
+btnClose.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+
+document.addEventListener('keydown', function(e) {
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});
